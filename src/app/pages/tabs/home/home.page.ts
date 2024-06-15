6df97776904621b4d4f570532bbf2a289f74ac53
samae28 <79955482+samae28@users.noreply.github.com>
2024-06-15 23:18:20 +0700

    only ongoing auction will be displayed on home tab

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { Artworks } from 'src/app/models/artworks.model';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'; // Imp
 
 @Component({
   selector: 'app-home',
@@ -47,11 +49,6 @@ export class HomePage implements OnInit {
       this.cdr.detectChanges();
     }, 2000);
   }
-
-  // openArtwork(artwork) {
-  //   this.router.navigate(['/tabs/artwork-detail', artwork.artworkID]);
-  // }
-
   getBanners() {
     this.apiService
       .getBanners()
@@ -70,9 +67,26 @@ export class HomePage implements OnInit {
       .valueChanges()
       .subscribe((data: Artworks[]) => {
         this.artwork = data;
-        this.auctionArtworks = this.artwork.filter(
-          (artwork) => artwork.isAuction === true || artwork.isAuction === 1
-        );
+
+        // Filter only ongoing auctions
+        this.auctionArtworks = this.artwork.filter((artwork) => {
+          if (artwork.isAuction && artwork.auction) {
+            const currentTime = new Date();
+            const startDate =
+              artwork.auction.startDate instanceof firebase.firestore.Timestamp
+                ? artwork.auction.startDate.toDate()
+                : artwork.auction.startDate;
+            const endDate =
+              artwork.auction.endDate instanceof firebase.firestore.Timestamp
+                ? artwork.auction.endDate.toDate()
+                : artwork.auction.endDate;
+
+            return currentTime >= startDate && currentTime <= endDate;
+          }
+          return false;
+        });
+
+        // Filter fixed price artworks
         this.fixedPriceArtworks = this.artwork.filter(
           (artwork) => artwork.isAuction === false || artwork.isAuction === 0
         );
